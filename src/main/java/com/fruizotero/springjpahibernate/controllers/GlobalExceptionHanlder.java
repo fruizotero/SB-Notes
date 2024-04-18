059128489ef64e3d04f360d1773137c6a95b0689
fruizotero <frank.git96@gmail.com>
2024-04-18 16:24:30 +0200

    (GlobalExceptionHanlder) Nuevos métodos para gestionar la excepciones.

--- src/main/java/com/fruizotero/springjpahibernate/controllers/GlobalExceptionHanlder.java
+++ src/main/java/com/fruizotero/springjpahibernate/controllers/GlobalExceptionHanlder.java
@@ -1,13 +1,16 @@
 package com.fruizotero.springjpahibernate.controllers;
 
+import com.fruizotero.springjpahibernate.exceptions.NotCreatedException;
 import com.fruizotero.springjpahibernate.exceptions.NotFoundException;
+import com.fruizotero.springjpahibernate.exceptions.NotUpdatedException;
 import com.fruizotero.springjpahibernate.utils.ApiResponse;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.ArrayList;
+import java.sql.SQLException;
 
 @ControllerAdvice
 public class GlobalExceptionHanlder {
@@ -15,13 +18,43 @@ public class GlobalExceptionHanlder {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ApiResponse<String>> handleNotFoundException(NotFoundException ex) {
 
-        return ResponseEntity.
-                status(HttpStatus.NOT_FOUND)
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
                 .body(ApiResponse.<String>builder()
                         .message(ex.getMessage())
                         .success(false)
-
                         .build());
 
+    }
+
+    @ExceptionHandler(NotCreatedException.class)
+    public ResponseEntity<ApiResponse<String>> handleNotCreatedException(NotCreatedException ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.<String>builder()
+                        .message(ex.getMessage())
+                        .success(false)
+                        .build());
+    }
+
+
+    @ExceptionHandler(NotUpdatedException.class)
+    public ResponseEntity<ApiResponse<String>> handleNotUpdatedException(NotUpdatedException ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.<String>builder()
+                        .message(ex.getMessage())
+                        .success(false)
+                        .build());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ApiResponse<String>> handleSqlException(SQLException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.<String>builder()
+                        .message(ex.getMessage())
+                        .success(false)
+                        .build());
     }
 }
