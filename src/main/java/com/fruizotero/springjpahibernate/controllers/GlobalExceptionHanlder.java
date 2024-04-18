416c5a65386938403880e5e52eae088288830622
fruizotero <frank.git96@gmail.com>
2024-04-18 21:45:09 +0200

    (GlobalExceptionHandler) Nuevo método para gestionar la excepciones de la validación de datos.

--- src/main/java/com/fruizotero/springjpahibernate/controllers/GlobalExceptionHanlder.java
+++ src/main/java/com/fruizotero/springjpahibernate/controllers/GlobalExceptionHanlder.java
@@ -3,6 +3,7 @@ package com.fruizotero.springjpahibernate.controllers;
 import com.fruizotero.springjpahibernate.exceptions.NotCreatedException;
 import com.fruizotero.springjpahibernate.exceptions.NotFoundException;
 import com.fruizotero.springjpahibernate.exceptions.NotUpdatedException;
+import com.fruizotero.springjpahibernate.exceptions.NotValidDataException;
 import com.fruizotero.springjpahibernate.utils.ApiResponse;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.sql.SQLException;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHanlder {
@@ -54,6 +56,17 @@ public class GlobalExceptionHanlder {
                 .status(HttpStatus.BAD_REQUEST)
                 .body(ApiResponse.<String>builder()
                         .message(ex.getMessage())
+                        .success(false)
+                        .build());
+    }
+
+    @ExceptionHandler(NotValidDataException.class)
+    public ResponseEntity<ApiResponse<Map<String, String>>> handleNotValidDataException(NotValidDataException ex) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(ApiResponse.<Map<String, String>>builder()
+                        .message(ex.getMessage())
+                        .data(ex.getErrors())
                         .success(false)
                         .build());
     }
